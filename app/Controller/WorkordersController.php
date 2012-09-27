dc0c0ccd38af31283305ede268265c9af420a4eb
maurozadu <maurozadu@gmail.com>
2012-09-27 17:15:33 -0300

    using compact() to set vars to the view

--- app/Controller/WorkordersController.php
+++ app/Controller/WorkordersController.php
@@ -6,9 +6,8 @@ class WorkordersController extends AppController {
 
 
 	public function index() {
-		$this->set(array(
-			'workorders' => $this->Workorder->getAll(array('manager_id' => 4)),
-		));
+		$workorders = $this->Workorder->getAll(array('manager_id' => 4));
+		$this->set(compact('workorders'));
 		$this->render('/Elements/workorders/index');
 	}
 
