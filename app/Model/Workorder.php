17c239babeffb52b2dc23e14a98acf8529e4a41a
maurozadu <maurozadu@gmail.com>
2012-09-27 16:48:18 -0300

    work data in the array before passing to the view

--- app/Model/Workorder.php
+++ app/Model/Workorder.php
@@ -17,9 +17,40 @@ class Workorder extends AppModel {
 		$findParams = array(
 			'conditions' => array(
 				'Workorder.manager_id' => $params['manager_id'],
+				'Workorder.active' => 1,
 			),
+			'contain' => array('Manager'),
 		);
-		return $this->find('all', $findParams);
+		$workorders = $this->find('all', $findParams);
+		$out = array();
+		foreach ($workorders as $workorder) {
+			$out[] = array(
+				'id' => $workorder['Workorder']['id'],
+				'slack_time' => $this->calculateSlackTime($workorder),
+				'status' => $workorder['Workorder']['status'],
+				'description' => $workorder['Workorder']['description'],
+				'owner' => $workorder['Manager']['username'],
+				'worktime' => $this->calculateWorkTime($workorder),
+			);
+		}
+		return $out;
+	}
+
+	/**
+	* function to calculate slack time, implementation pending
+	* @return slack time in seconds
+	*/
+	public function calculateSlackTime($workorder) {
+		return rand(0, 9999999);
+	}
+
+
+	/**
+	* function to calculate work time, implementation pending
+	* @return slack time in seconds
+	*/
+	public function calculateWorkTime($workorder) {
+		return rand(0, 90000);
 	}
 
 }
--- app/View/Elements/workorders/all.ctp
+++ app/View/Elements/workorders/all.ctp
@@ -1,2 +1,20 @@
-<?php
-pr($workorders);
+<table>
+	<tr>
+		<th>id</th>
+		<th>slack_time</th>
+		<th>status</th>
+		<th>description</th>
+		<th>owner</th>
+		<th>worktime</th>
+	</tr>
+<?php foreach ($workorders as $workorder): ?>
+	<tr>
+		<td><?php echo $workorder['id']; ?></td>
+		<td><?php echo gmdate('d\d H\h i\m', $workorder['slack_time']); ?></td>
+		<td><?php echo $workorder['status']; ?></td>
+		<td><?php echo $workorder['description']; ?></td>
+		<td><?php echo $workorder['owner']; ?></td>
+		<td><?php echo gmdate('H\h i\m', $workorder['worktime']); ?></td>
+	</tr>
+<?php endforeach; ?>
+</table>
