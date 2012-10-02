da37d3961db554f8832af645385d8c2f5a02c527
maurozadu <maurozadu@gmail.com>
2012-10-02 11:35:04 -0300

    workorders dashboard action created

--- app/Controller/WorkordersController.php
+++ app/Controller/WorkordersController.php
@@ -5,6 +5,12 @@ class WorkordersController extends AppController {
 	public $scaffold;
 
 
+	public function dashboard() {
+		$workorders = $this->Workorder->getAll(array('manager_id' => 4));
+		$this->set(compact('workorders'));
+	}
+
+
 	public function index() {
 		$workorders = $this->Workorder->getAll(array('manager_id' => 4));
 		$this->set(compact('workorders'));
