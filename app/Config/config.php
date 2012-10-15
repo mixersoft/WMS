ce8581bb5fde4800cffca3bad6a8db4920233bda
maurozadu <maurozadu@gmail.com>
2012-10-15 14:54:27 -0300

    move all configurations to another file. ref #14

---
+++ app/Config/config.php
@@ -0,0 +1,31 @@
+<?php
+
+$isLocal = (env('REMOTE_ADDR') == '127.0.0.1'or substr(env('HTTP_HOST'), -4) == '.dev'or class_exists('ShellDispatcher'));
+
+$config = array(
+	'isLocal' => $isLocal,
+	'debug' => $isLocal ? 2 : 2,
+	'Error' => array(
+		'handler' => 'ErrorHandler::handleError',
+		'level' => E_ALL & ~E_DEPRECATED,
+		'trace' => true
+	),
+	'Exception' => array(
+		'handler' => 'ErrorHandler::handleException',
+		'renderer' => 'ExceptionRenderer',
+		'log' => true
+	),
+	'App.encoding' => 'UTF-8',
+	'Session' => array(
+		'defaults' => 'php'
+	),
+	'Security' => array(
+		'level' => 'medium',
+		'salt' => '559csfneabpchbaapfpci914d21ab41e3a3da0b9f',
+		'cipherSeed' => '0685919657453555424966736845',
+	),
+	'Acl' => array(
+		'classname' => 'DbAcl',
+		'database' => 'default',
+	),
+ );
