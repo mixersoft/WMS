1cc6b78126f49cb2993342462685c491a7d6dc07
maurozadu <maurozadu@gmail.com>
2012-10-19 11:37:23 -0300

    cake core configrations moved back to the standard way. close #14

--- app/Config/config.php
+++ app/Config/config.php
@@ -8,29 +8,5 @@ $isLocal = (
 
 $config = array(
 	'isLocal' => $isLocal,
-	'debug' => $isLocal ? 2 : 2,
-	'Error' => array(
-		'handler' => 'ErrorHandler::handleError',
-		'level' => E_ALL & ~E_DEPRECATED,
-		'trace' => true
-	),
-	'Exception' => array(
-		'handler' => 'ErrorHandler::handleException',
-		'renderer' => 'ExceptionRenderer',
-		'log' => true
-	),
-	'App.encoding' => 'UTF-8',
-	'Session' => array(
-		'defaults' => 'php'
-	),
-	'Security' => array(
-		'level' => 'medium',
-		'salt' => '559csfneabpchbaapfpci914d21ab41e3a3da0b9f',
-		'cipherSeed' => '0685919657453555424966736845',
-	),
-	'Acl' => array(
-		'classname' => 'DbAcl',
-		'database' => 'default',
-	),
 	'host_PES' => $isLocal ? 'snappi-dev' : 'dev.snaphappi.com',
 );
